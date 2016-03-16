97a9f21fba9d1cd7f3294f49feef54c8f66469fe
elenaaralla <elena.aralla@gmail.com>
2016-03-16 16:05:08 +0100

    test

--- js/index.js
+++ js/index.js
@@ -3,4 +3,5 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
     console.log("Device ready!");
+	$("#deviceInfo").html("Device Name: "+device.name+"<br />Device Cordova:"+device.cordova+"<br />Device Platform:"+device.platform+"<br />Device UUID:"+device.uuid+"<br />Device Version:"+device.version);     
 }
