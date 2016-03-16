d6e1bc5e6385deff1fc4a2a27cc0684a1ee49a3d
elenaaralla <elena.aralla@gmail.com>
2016-03-16 17:28:10 +0100

    accelerometer use test

--- js/index.js
+++ js/index.js
@@ -9,5 +9,27 @@ function onDeviceReady() {
 
     console.log("Device ready!");
 	$("#deviceInfo").html("Device Name: "+device.name+"<br />Device Cordova:"+device.cordova+"<br />Device Platform:"+device.platform+"<br />Device UUID:"+device.uuid+"<br />Device Version:"+device.version);     
+
+	var acceler = navigator.accelerometer || null;
+
+	if(acceler)
+	{
+		acceler.getCurrentAcceleration(onSuccess, onError);
+	}
+	else
+	{
+		console.log("accelerometer");
+	}
+}
+
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+}
+
+function onError() {
+    alert('onError!');
 }
 
